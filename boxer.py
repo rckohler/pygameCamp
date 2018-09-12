00f3d1441477654af7ef2d20044f7c219d67c76d
rckohler <tfbalc@gmail.com>
2018-09-12 12:42:33 -0700

    correct

--- boxer.py
+++ boxer.py
@@ -18,29 +18,46 @@ class ListObject:
         print(self.name + ": enact error")
 
 class Attack(ListObject):
-    def __init__(self, attackName, accuracy, power):
+    def __init__(self, attackName, description, accuracy, power):
         self.name = attackName
-    def enact(self):
-        print(self.name + ":default enact")
-        return 1
+        self.description = description
+        self.accuracy = accuracy
+        self.power = power
+    def enact(self, attacker, defender):
+        print(attacker.name + self.description + " at " + defender.name)
 
+        attackRoll = random.randint(1,self.accuracy)
+        print(attackRoll)
+        if attackRoll == 1:
+            print ("miss")
+        else:
+            print("The blow  lands.")
+            damage = random.randint(1,self.power)
+            print("Dealing " + str(damage) + " damage.")
+            defender.takeDamage(damage)
 
 class Boxer:
     def __init__(self, name, player = False):
         self.name = name
         self.player = player
         self.hitsRemainig = 10
-        punch = Attack("punch")
-        kick = Attack("kick")
+        self.accuracy = 5
+        self.damage = 3
+        punch = Attack("p","punch",self.accuracy + 1,1)
+        kick = Attack("k","kick",self.accuracy + 1,1)
         self.attacks = [punch, kick]
-    def chooseAttack(self):
+    def chooseAttack(self, attacker, defender):
         if self.player:
-            action =  getObjectFromList("Choose an attack",self.attacks)
-            ret = action.enact()
+            action =  getObjectFromList(self.name + " choose an attack.", self.attacks)
+            ret = action.enact(attacker,defender)
             return ret
         else:
-            result = random.choice(self.attacks).enact()
+            result = random.choice(self.attacks).enact(attacker,defender)
             return result
+    def takeDamage(self, damage):
+        print("taking damage")
+        self.hitsRemainig-= damage
+
 def getObjectFromList(prompt, list):
     counter = 1
     for object in list:
@@ -52,6 +69,6 @@ def game():
     a = Boxer("Abe",True)
     b = Boxer("Bob")
     while a.hitsRemainig > 0 and b.hitsRemainig > 0:
-        b.hitsRemainig -=a.chooseAttack()
-        a.hitsRemainig -=b.chooseAttack()
+        a.chooseAttack(a,b)
+        b.chooseAttack(b,a)
 game()
